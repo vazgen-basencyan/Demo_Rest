9f770342c920e052d3881f0c0076b0198b2ecb8d
vazgen-basencyan <63777526+vazgen-basencyan@users.noreply.github.com>
2021-02-19 19:40:54 +0400

    Update settings.py

--- Demo_Rest/settings.py
+++ Demo_Rest/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'Demo_Rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mynetwork',
-        'USER': 'mynetworkuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
